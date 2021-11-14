78d7a7a5234ec051e5eb1556db7f9525b3e7a299
Mat-Carv <mat.candido2012@gmail.com>
2021-11-14 22:10:09 +1100

    Created Packs table. Belongs to User(owner/author); User has Many Packs. Belongs to Category; Category has Many Packs.

---
+++ app/models/pack.rb
@@ -0,0 +1,4 @@
+class Pack < ApplicationRecord
+  belongs_to :user
+  belongs_to :category
+end
---
+++ db/migrate/20211114104449_create_packs.rb
@@ -0,0 +1,13 @@
+class CreatePacks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :packs do |t|
+      t.string :name
+      t.text :description
+      t.float :price
+      t.references :user, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
