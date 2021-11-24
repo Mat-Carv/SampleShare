5868012230f53d6b294e6d21c827d138a1e6c9d5
Mat-Carv <mat.candido2012@gmail.com>
2021-11-24 12:34:31 +1100

    Coded seed file. 100 users and 1 pack for each user.

--- app/controllers/catalogues_controller.rb
+++ app/controllers/catalogues_controller.rb
@@ -2,7 +2,7 @@ class CataloguesController < ApplicationController
 
     def index
         # @packs holds only packs the user hasn't created or purchased
-        @packs = Pack.all - current_user.packs - current_user.purchased_packs
+        @packs = Pack.all # - current_user.packs - current_user.purchased_packs
         
         @category = Category.all
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+100.times{User.create(Faker::Internet.user('username', 'email', 'password'))}
+
+User.all.each{|user|
+    user.packs.create([
+        name: Faker::Book.title , 
+        description: Faker::Lorem.sentence(word_count: 6) , 
+        price: Faker::Number.decimal(l_digits: 2) ,
+        category_id: Faker::Number.within(range: 1..8),
+        available: true  ])
+}
